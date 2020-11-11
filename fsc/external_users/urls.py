b66df2e412afc15afda381815bf1dc0485a72174
venkat215 <srtvenkat215@gmail.com>
2020-11-11 07:07:40 +0530

    removed jwt import

--- fsc/external_users/urls.py
+++ fsc/external_users/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-# from .views import RegisterView, LoginView
-from rest_framework_simplejwt import views as jwt_views
 from .views import ReceiveMessageView
 
 urlpatterns = [
