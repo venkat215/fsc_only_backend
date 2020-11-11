227ad478d119335dce26a2ab8f5fa197182c3d11
venkat215 <srtvenkat215@gmail.com>
2020-11-11 07:21:54 +0530

    changed build to react build for gitignore

--- fsc/fsc/settings.py
+++ fsc/fsc/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'fsc.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'build')],
+        'DIRS': [os.path.join(BASE_DIR, 'react_build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,7 +131,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "build/static"),
+    os.path.join(BASE_DIR, "react_build/static"),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
